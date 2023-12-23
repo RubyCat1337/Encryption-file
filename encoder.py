2f1aa69f2591a9157bca7dc8140266ff95b03250
Виталя Базукин <103893102+RubyCat1337@users.noreply.github.com>
2023-12-23 21:44:49 +0200

    Update encoder.py

--- encoder.py
+++ encoder.py
@@ -14,7 +14,6 @@ def encrypt(data, key):
     :return: Зашифрованные данные.
     :rtype: bytes
     """
-
     if not isinstance(data, bytes):
         raise TypeError("Данные для шифрования должны быть байтами")
 
@@ -27,22 +26,44 @@ def encrypt(data, key):
     cipher = AES.new(key, AES.MODE_CBC)
     encrypted_data = cipher.encrypt(pad(data, AES.block_size))
 
-    # Сохранить ключ для шифрования
-    with open("encryption_key.bin", "wb") as file:
-        file.write(key)
+    # Сохранение ключа для шифрования в безопасном формате
+    with open("encryption_key.bin", "wb") as key_file:
+        key_file.write(key)
 
     return encrypted_data
-    
+
+
+def decrypt(encrypted_data, key, iv):
+    """
+    Функция для дешифратор данных на 512 битах с использованием AES.
+
+    :param data: Бинарные данные для шифрования.
+    :type data: bytes
+    :param key: Ключ для AES шифрования. Должен быть длиной 64 байта.
+    :type key: bytes
+    :return: Зашифрованные данные.
+    :rtype: bytes
+    """
+            # Создание объекта AES для дешифрования с использованием предоставленного ключа и вектора инициализации (IV)
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+        # Расшифровка данных с использованием AES в режиме CBC
+    decrypted_data = unpad(cipher.decrypt(encrypted_data), AES.block_size)
+        # Возврат расшифрованных данных
+    return decrypted_data
 
 
 
 # Пример использования:
-# укажіте файл для шыврования 
-file_path = "C:/файл"
+file_path = "C:/путь/к/файлу"
 with open(file_path, 'rb') as file:
     data_to_encrypt = file.read()
 
-encryption_key = get_random_bytes(64)  # Генерация случайного ключа длиной 64 байта (512 бит)
+encryption_key = get_random_bytes(64)
 encrypted_data = encrypt(data_to_encrypt, encryption_key)
 
+
+# Генерация случайного вектора инициализации (IV) длиной, соответствующей размеру блока шифрования AES
+iv = get_random_bytes(AES.block_size)
+# Расшифровка зашифрованных данных, используя предоставленный ключ и сгенерированный вектор инициализации
+decrypted_data = decrypt(encrypted_data, encryption_key, iv)
 
