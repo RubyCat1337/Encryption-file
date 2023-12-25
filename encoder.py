c0b5d140a464c4d09e66ad42c7525211650456d0
Виталя Базукин <103893102+RubyCat1337@users.noreply.github.com>
2023-12-25 11:17:33 +0200

    Update encoder.py

--- encoder.py
+++ encoder.py
@@ -4,21 +4,21 @@ from Crypto.Util.Padding import pad, unpad
 
 def encrypt(data, key):
     """
-    Function to encrypt data using AES with a 512-bit key.
+    Функція для шифрування даних за допомогою AES з ключем 512 біт.
 
-    :param data: Binary data to be encrypted.
+    :param data: Бінарні дані для шифрування.
     :type data: bytes
-    :param key: Key for AES encryption. Must be 64 bytes long.
+    :param key: Ключ для шифрування AES. Має бути довжиною 64 байти.
     :type key: bytes
-    :return: Encrypted data.
+    :return: Зашифровані дані.
     :rtype: bytes
     """
     if not isinstance(data, bytes):
-        raise TypeError("Data to encrypt must be bytes")
+        raise TypeError("Дані для шифрування повинні бути байтами")
 
     if not isinstance(key, bytes) or len(key) != 32:
-        raise ValueError("Encryption key must be 32 bytes long")
-    
+        raise ValueError("Ключ шифрування повинен мати довжину 32 байти")
+
     cipher = AES.new(key, AES.MODE_CBC)
     encrypted_data = cipher.encrypt(pad(data, AES.block_size))
 
@@ -29,25 +29,25 @@ def encrypt(data, key):
 
 def decrypt(encrypted_data, key, iv):
     """
-    Function to decrypt data using AES with a 512-bit key.
+    Функція для розшифрування даних за допомогою AES з ключем 512 біт.
 
-    :param encrypted_data: Binary data to be decrypted.
+    :param encrypted_data: Бінарні дані для розшифрування.
     :type encrypted_data: bytes
-    :param key: Key for AES encryption. Must be 64 bytes long.
+    :param key: Ключ для шифрування AES. Має бути довжиною 64 байти.
     :type key: bytes
-    :param iv: Initialization vector (IV) for AES decryption.
+    :param iv: Вектор ініціалізації (IV) для розшифрування AES.
     :type iv: bytes
-    :return: Decrypted data.
+    :return: Розшифровані дані.
     :rtype: bytes
     """
     cipher = AES.new(key, AES.MODE_CBC, iv)
     decrypted_data = unpad(cipher.decrypt(encrypted_data), AES.block_size)
     return decrypted_data
 
-# Example usage:
+# Приклад використання:
 file_path = r"C:\Users\max-BP\Desktop\sss.txt"
 with open(file_path, 'rb') as file:
-    data_to_encrypt = pad(file.read(), AES.block_size)  # Ensure data is padded
+    data_to_encrypt = pad(file.read(), AES.block_size)  # Забезпечте відступ для даних
 
 encryption_key = get_random_bytes(32)
 encrypted_data = encrypt(data_to_encrypt, encryption_key)
@@ -55,16 +55,19 @@ encrypted_data = encrypt(data_to_encrypt, encryption_key)
 with open("encrypted_file.bin", "wb") as encrypted_file:
     encrypted_file.write(encrypted_data)
 
-# Save the key used for file encryption to a separate file
+# Збереження ключа, використаного для шифрування файлу, у окремому файлі
 with open("file_encryption_key.bin", "wb") as key_file:
     key_file.write(encryption_key)
 
-# Example of decrypting the encryption key file
+# Приклад розшифрування файлу з ключем шифрування
 with open("encrypted_file.bin", "rb") as encrypted_file:
     encrypted_data_to_decrypt = encrypted_file.read()
 
 with open("file_encryption_key.bin", "rb") as key_file:
     key_for_decryption = key_file.read()
 
-# Decrypt the data
+# Розшифрування даних
 decrypted_data = decrypt(encrypted_data_to_decrypt, key_for_decryption, get_random_bytes(AES.block_size))
+
+with open("decrypted_file.txt", "wb") as decrypted_file:
+    decrypted_file.write(decrypted_data)
