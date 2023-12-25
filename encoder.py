2407a6a0dfbe7591a6d1611ae8d5bb283867fdc2
Виталя Базукин <103893102+RubyCat1337@users.noreply.github.com>
2023-12-25 10:53:13 +0200

    Update encoder.py

--- encoder.py
+++ encoder.py
@@ -2,68 +2,69 @@ from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 from Crypto.Util.Padding import pad, unpad
 
-
 def encrypt(data, key):
     """
-    Функция для шифрования данных на 512 битах с использованием AES.
+    Function to encrypt data using AES with a 512-bit key.
 
-    :param data: Бинарные данные для шифрования.
+    :param data: Binary data to be encrypted.
     :type data: bytes
-    :param key: Ключ для AES шифрования. Должен быть длиной 64 байта.
+    :param key: Key for AES encryption. Must be 64 bytes long.
     :type key: bytes
-    :return: Зашифрованные данные.
+    :return: Encrypted data.
     :rtype: bytes
     """
     if not isinstance(data, bytes):
-        raise TypeError("Данные для шифрования должны быть байтами")
+        raise TypeError("Data to encrypt must be bytes")
 
-    if not isinstance(key, bytes):
-        raise TypeError("Ключ для шифрования должен быть байтами")
-
-    if len(data) % AES.block_size != 0:
-        raise ValueError("Размер данных для шифрования должен быть кратен размеру блока шифрования AES (16 байтов)")
-
+    if not isinstance(key, bytes) or len(key) != 32:
+        raise ValueError("Encryption key must be 32 bytes long")
+    
     cipher = AES.new(key, AES.MODE_CBC)
     encrypted_data = cipher.encrypt(pad(data, AES.block_size))
 
-    # Сохранение ключа для шифрования в безопасном формате
     with open("encryption_key.bin", "wb") as key_file:
         key_file.write(key)
 
     return encrypted_data
 
-
 def decrypt(encrypted_data, key, iv):
     """
-    Функция для дешифратор данных на 512 битах с использованием AES.
+    Function to decrypt data using AES with a 512-bit key.
 
-    :param data: Бинарные данные для шифрования.
-    :type data: bytes
-    :param key: Ключ для AES шифрования. Должен быть длиной 64 байта.
+    :param encrypted_data: Binary data to be decrypted.
+    :type encrypted_data: bytes
+    :param key: Key for AES encryption. Must be 64 bytes long.
     :type key: bytes
-    :return: Зашифрованные данные.
+    :param iv: Initialization vector (IV) for AES decryption.
+    :type iv: bytes
+    :return: Decrypted data.
     :rtype: bytes
     """
-            # Создание объекта AES для дешифрования с использованием предоставленного ключа и вектора инициализации (IV)
     cipher = AES.new(key, AES.MODE_CBC, iv)
-        # Расшифровка данных с использованием AES в режиме CBC
     decrypted_data = unpad(cipher.decrypt(encrypted_data), AES.block_size)
-        # Возврат расшифрованных данных
     return decrypted_data
 
-
-
-# Пример использования:
-file_path = "C:/путь/к/файлу"
+# Example usage:
+file_path = r"C:\Users\max-BP\Desktop\sss.txt"
 with open(file_path, 'rb') as file:
-    data_to_encrypt = file.read()
+    data_to_encrypt = pad(file.read(), AES.block_size)  # Ensure data is padded
 
-encryption_key = get_random_bytes(64)
+encryption_key = get_random_bytes(32)
 encrypted_data = encrypt(data_to_encrypt, encryption_key)
 
+with open("encrypted_file.bin", "wb") as encrypted_file:
+    encrypted_file.write(encrypted_data)
 
-# Генерация случайного вектора инициализации (IV) длиной, соответствующей размеру блока шифрования AES
-iv = get_random_bytes(AES.block_size)
-# Расшифровка зашифрованных данных, используя предоставленный ключ и сгенерированный вектор инициализации
-decrypted_data = decrypt(encrypted_data, encryption_key, iv)
+# Save the key used for file encryption to a separate file
+with open("file_encryption_key.bin", "wb") as key_file:
+    key_file.write(encryption_key)
 
+# Example of decrypting the encryption key file
+with open("encrypted_file.bin", "rb") as encrypted_file:
+    encrypted_data_to_decrypt = encrypted_file.read()
+
+with open("file_encryption_key.bin", "rb") as key_file:
+    key_for_decryption = key_file.read()
+
+# Decrypt the data
+decrypted_data = decrypt(encrypted_data_to_decrypt, key_for_decryption, get_random_bytes(AES.block_size))
